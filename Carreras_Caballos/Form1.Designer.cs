b8d1f9d552be504563202154255eee5f18cd71c1
VATOTORO <vatotoroingesoft@gmail.com>
2016-04-21 19:14:12 -0500

    Primer commit

---
+++ Carreras_Caballos/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Carreras_Caballos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panelCarrera = new System.Windows.Forms.Panel();
+            this.labelTotal = new System.Windows.Forms.Label();
+            this.comboBoxCaballo = new System.Windows.Forms.ComboBox();
+            this.labelSaldoTotal = new System.Windows.Forms.Label();
+            this.labelApuesta = new System.Windows.Forms.Label();
+            this.textBoxApuesta = new System.Windows.Forms.TextBox();
+            this.labelSelectCaballo = new System.Windows.Forms.Label();
+            this.botonIniciar = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBoxBase = new System.Windows.Forms.ComboBox();
+            this.labelBase = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panelCarrera
+            // 
+            this.panelCarrera.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelCarrera.BackgroundImage")));
+            this.panelCarrera.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelCarrera.Location = new System.Drawing.Point(12, 12);
+            this.panelCarrera.Name = "panelCarrera";
+            this.panelCarrera.Size = new System.Drawing.Size(917, 353);
+            this.panelCarrera.TabIndex = 0;
+            this.panelCarrera.Paint += new System.Windows.Forms.PaintEventHandler(this.panelCarrera_Paint);
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(967, 36);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(154, 21);
+            this.labelTotal.TabIndex = 1;
+            this.labelTotal.Text = "SALDO TOTAL";
+            this.labelTotal.Visible = false;
+            // 
+            // comboBoxCaballo
+            // 
+            this.comboBoxCaballo.AccessibleName = "";
+            this.comboBoxCaballo.FormattingEnabled = true;
+            this.comboBoxCaballo.Items.AddRange(new object[] {
+            "SELECCIONAR",
+            "Caballo 1",
+            "Caballo 2",
+            "Caballo 3",
+            "Caballo 4"});
+            this.comboBoxCaballo.Location = new System.Drawing.Point(990, 266);
+            this.comboBoxCaballo.Name = "comboBoxCaballo";
+            this.comboBoxCaballo.Size = new System.Drawing.Size(107, 21);
+            this.comboBoxCaballo.TabIndex = 2;
+            this.comboBoxCaballo.Visible = false;
+            // 
+            // labelSaldoTotal
+            // 
+            this.labelSaldoTotal.AutoSize = true;
+            this.labelSaldoTotal.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSaldoTotal.Location = new System.Drawing.Point(1010, 75);
+            this.labelSaldoTotal.Name = "labelSaldoTotal";
+            this.labelSaldoTotal.Size = new System.Drawing.Size(59, 19);
+            this.labelSaldoTotal.TabIndex = 3;
+            this.labelSaldoTotal.Text = "$5000";
+            this.labelSaldoTotal.Visible = false;
+            // 
+            // labelApuesta
+            // 
+            this.labelApuesta.AutoSize = true;
+            this.labelApuesta.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelApuesta.Location = new System.Drawing.Point(991, 124);
+            this.labelApuesta.Name = "labelApuesta";
+            this.labelApuesta.Size = new System.Drawing.Size(109, 21);
+            this.labelApuesta.TabIndex = 4;
+            this.labelApuesta.Text = "APUESTA";
+            this.labelApuesta.Visible = false;
+            // 
+            // textBoxApuesta
+            // 
+            this.textBoxApuesta.Location = new System.Drawing.Point(993, 173);
+            this.textBoxApuesta.Name = "textBoxApuesta";
+            this.textBoxApuesta.Size = new System.Drawing.Size(105, 20);
+            this.textBoxApuesta.TabIndex = 5;
+            this.textBoxApuesta.Visible = false;
+            this.textBoxApuesta.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxApuesta_KeyPress);
+            // 
+            // labelSelectCaballo
+            // 
+            this.labelSelectCaballo.AutoSize = true;
+            this.labelSelectCaballo.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSelectCaballo.Location = new System.Drawing.Point(963, 213);
+            this.labelSelectCaballo.Name = "labelSelectCaballo";
+            this.labelSelectCaballo.Size = new System.Drawing.Size(164, 19);
+            this.labelSelectCaballo.TabIndex = 6;
+            this.labelSelectCaballo.Text = "Seleccionar caballo";
+            this.labelSelectCaballo.Visible = false;
+            // 
+            // botonIniciar
+            // 
+            this.botonIniciar.Font = new System.Drawing.Font("Cooper Black", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botonIniciar.Location = new System.Drawing.Point(937, 325);
+            this.botonIniciar.Name = "botonIniciar";
+            this.botonIniciar.Size = new System.Drawing.Size(211, 40);
+            this.botonIniciar.TabIndex = 7;
+            this.botonIniciar.Text = "Iniciar carrera";
+            this.botonIniciar.UseVisualStyleBackColor = true;
+            this.botonIniciar.Visible = false;
+            this.botonIniciar.Click += new System.EventHandler(this.botonIniciar_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBoxBase
+            // 
+            this.comboBoxBase.FormattingEnabled = true;
+            this.comboBoxBase.Items.AddRange(new object[] {
+            "5000",
+            "10000",
+            "20000",
+            "30000",
+            "40000",
+            "50000"});
+            this.comboBoxBase.Location = new System.Drawing.Point(981, 193);
+            this.comboBoxBase.Name = "comboBoxBase";
+            this.comboBoxBase.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxBase.TabIndex = 8;
+            this.comboBoxBase.SelectedIndexChanged += new System.EventHandler(this.comboBoxBase_SelectedIndexChanged);
+            // 
+            // labelBase
+            // 
+            this.labelBase.Font = new System.Drawing.Font("Cooper Black", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBase.Location = new System.Drawing.Point(935, 128);
+            this.labelBase.Name = "labelBase";
+            this.labelBase.Size = new System.Drawing.Size(214, 42);
+            this.labelBase.TabIndex = 9;
+            this.labelBase.Text = "Escoger saldo base para iniciar el juego";
+            this.labelBase.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 377);
+            this.Controls.Add(this.labelBase);
+            this.Controls.Add(this.comboBoxBase);
+            this.Controls.Add(this.botonIniciar);
+            this.Controls.Add(this.comboBoxCaballo);
+            this.Controls.Add(this.labelSelectCaballo);
+            this.Controls.Add(this.textBoxApuesta);
+            this.Controls.Add(this.labelApuesta);
+            this.Controls.Add(this.labelSaldoTotal);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.panelCarrera);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelCarrera;
+        private System.Windows.Forms.Label labelTotal;
+        private System.Windows.Forms.ComboBox comboBoxCaballo;
+        private System.Windows.Forms.Label labelSaldoTotal;
+        private System.Windows.Forms.Label labelApuesta;
+        private System.Windows.Forms.TextBox textBoxApuesta;
+        private System.Windows.Forms.Label labelSelectCaballo;
+        private System.Windows.Forms.Button botonIniciar;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ComboBox comboBoxBase;
+        private System.Windows.Forms.Label labelBase;
+    }
+}
+
---
+++ Carreras_Caballos/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carreras_Caballos
+{
+    public partial class Form1 : Form
+    {
+        /*Posicion de los elemento*/
+        private int posx1;
+        private int posy1;
+        private int posx2;
+        private int posy2;
+        private int posx3;
+        private int posy3;
+        private int posx4;
+        private int posy4;
+        //Tamaño de la imagen
+        private int ancho;
+        private int alto;
+        /*Indica el cambio que tendra el elemento*/
+        private int vel1;
+        private int vel2;
+        private int vel3;
+        private int vel4;
+        //Se crean los hilos
+        Thread hilo;
+        Thread hilo2;
+        Thread hilo3;
+        Thread hilo4;
+        //Variable para saber si gano algunos de los caballos 
+        private String finCaballo;
+        //Suma total del saldo del jugador
+        double saldoTotal;
+        double saldoApostar;
+        String caballoApuesta;
+        Boolean estadoHilo = true;
+
+        Random r = new Random();
+
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        Random rand3 = new Random();
+        Random rand4 = new Random();
+
+        //CONSTRUCTOR
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxCaballo.SelectedIndex = 0;
+            posx1 = 0;
+            posy1 = 30;
+            posx2 = 0;
+            posy2 = 110;
+            posx3 = 0;
+            posy3 = 190;
+            posx4 = 0;
+            posy4 = 260;
+            ancho = 70;
+            alto = 50;
+        }
+
+        public void estadoComponentesPanel()
+        {
+            labelTotal.Visible = false;
+            labelSaldoTotal.Visible = false;
+            labelApuesta.Visible = false;
+            textBoxApuesta.Visible = false;
+            labelSelectCaballo.Visible = false;
+            comboBoxCaballo.Visible = false;
+            botonIniciar.Visible = false;
+
+            labelBase.Visible = true;
+            comboBoxBase.Visible = true;
+        }
+
+        /*Evento del Metodo que es invocado al seleccionar un item del comboBoxBase, 
+         * activa los demas componentes faltantes de la vista.*/
+        private void comboBoxBase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            saldoTotal = Convert.ToDouble(comboBoxBase.SelectedItem);
+            labelSaldoTotal.Text = "$" + Convert.ToString(saldoTotal);
+            //Se desactivan los dos componentes, comboBoxBase y labelBase.
+            labelBase.Visible = false;
+            comboBoxBase.Visible = false;
+            //Se activan el resto de componentes de la vista.
+            labelTotal.Visible = true;
+            labelSaldoTotal.Visible = true;
+            labelApuesta.Visible = true;
+            textBoxApuesta.Visible = true;
+            labelSelectCaballo.Visible = true;
+            comboBoxCaballo.Visible = true;
+            botonIniciar.Visible = true;
+        }
+
+        /*Evento del metodo para iniciar juego*/
+        private void botonIniciar_Click(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(textBoxApuesta.Text) || comboBoxCaballo.SelectedIndex == 0))
+            {
+                //Apuesta del jugador
+                saldoApostar = Convert.ToDouble(textBoxApuesta.Text);
+                //Caballo que escogio el jugador
+                caballoApuesta = comboBoxCaballo.SelectedItem.ToString();
+
+                if (saldoApostar <= saldoTotal)
+                {
+                    estadoHilo = true;
+                    iniciarHilos(true);
+                }
+                else
+                {
+                    MessageBox.Show("El saldo apostar supera tu saldo total", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    textBoxApuesta.Text = " ";
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe completar la informacion", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        public void repintar()
+        {
+            while (estadoHilo)
+            {
+                vel1 = rand1.Next(r.Next(30));
+
+                if (panelCarrera.Width > posx1 + ancho)
+                {
+                    posx1 = posx1 + vel1; 
+                }
+                else
+                {
+                    estadoHilo = false;
+                    finCaballo = "Caballo 1";
+                    calificarCarrera(finCaballo);
+                }
+                //Repinta la interfaz 
+                panelCarrera.Invalidate();
+                //Duerme el hilo
+                Thread.Sleep(100);
+            }
+        }
+
+        public void repintar2()
+        {
+            while (estadoHilo)
+            {
+                vel2 = rand2.Next(r.Next(30));
+
+                if (panelCarrera.Width > posx2 + ancho)
+                {
+                    posx2 = posx2 + vel2;
+                }
+                else
+                {
+                    estadoHilo = false;
+                    finCaballo = "Caballo 2";
+                    calificarCarrera(finCaballo);  
+                }
+                //Repinta la interfaz 
+                panelCarrera.Invalidate();
+                //Duerme el hilo
+                Thread.Sleep(100);
+            }
+        }
+
+        public void repintar3()
+        {
+            while (estadoHilo)
+            {
+                vel3 = rand3.Next(r.Next(30));
+
+                if (panelCarrera.Width > posx3 + ancho)
+                {
+                    posx3 = posx3 + vel3;
+                }
+                else
+                {
+                    estadoHilo = false;
+                    finCaballo = "Caballo 3";
+                    calificarCarrera(finCaballo);
+                }
+                //Repinta la interfaz 
+                panelCarrera.Invalidate();
+                //Duerme el hilo
+                Thread.Sleep(100);
+            }
+        }
+
+        public void repintar4()
+        {
+            while (estadoHilo)
+            {
+                vel4 = rand4.Next(r.Next(30));
+                if (panelCarrera.Width > posx4 + ancho)
+                {
+                    posx4 = posx4 + vel4;
+                }
+                else
+                {
+                    estadoHilo = false;
+                    finCaballo = "Caballo 4";
+                    calificarCarrera(finCaballo);
+                }
+                //Repinta la interfaz 
+                panelCarrera.Invalidate();
+                //Duerme el hilo
+                Thread.Sleep(100);
+            }
+        }
+
+        private void iniciarHilos(Boolean estado)
+        {
+            if (estado == true)
+            {
+                hilo = new Thread(repintar);
+                hilo.Start();
+
+                hilo2 = new Thread(repintar2);
+                hilo2.Start();
+
+                hilo3 = new Thread(repintar3);
+                hilo3.Start();
+
+                hilo4 = new Thread(repintar4);
+                hilo4.Start();
+            }
+        }
+
+        public void calificarCarrera(String caballo)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            if (caballo.Equals(caballoApuesta))
+            {
+                MessageBox.Show("SU CABALLO HA GANADO", null, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                saldoTotal = saldoTotal + saldoApostar;
+                labelSaldoTotal.Text = "$"+saldoTotal+"";
+                reiniciarCaballos();
+            }
+            else
+            {
+                MessageBox.Show("SU CABALLO HA PERDIDO", null, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                saldoTotal = saldoTotal - saldoApostar;
+                labelSaldoTotal.Text = "$" + saldoTotal + "";
+                if (saldoTotal == 0) 
+                {
+                    MessageBox.Show("SE HA QUEDADO SIN SALDO PARA JUGAR, CARGA MAS SALDO A TU JUEGO", null, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    estadoComponentesPanel();
+                }
+                reiniciarCaballos();
+                
+            }
+        }
+
+        public void reiniciarCaballos()
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            comboBoxCaballo.SelectedIndex = 0;
+            textBoxApuesta.Text = " ";
+            posx1 = 0;
+            posy1 = 30;
+            posx2 = 0;
+            posy2 = 110;
+            posx3 = 0;
+            posy3 = 190;
+            posx4 = 0;
+            posy4 = 260;
+            //Repinta la interfaz 
+            panelCarrera.Invalidate();
+        }
+
+        private void panelCarrera_Paint(object sender, PaintEventArgs e)
+        {
+            /*Recordar cambiar la propiedad de la imagen a cambiar si es posterior para 
+             * que pueda refrescar la imagen, sin esto no la pinta*/
+            e.Graphics.DrawImage(new Bitmap("horse.png"), posx1, posy1, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("horse.png"), posx2, posy2, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("horse.png"), posx3, posy3, ancho, alto);
+            e.Graphics.DrawImage(new Bitmap("horse.png"), posx4, posy4, ancho, alto);
+        }
+
+        private void textBoxApuesta_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        } 
+    }
+}
